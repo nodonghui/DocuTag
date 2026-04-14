67c6f5e843a4a51f0727b80dbdaae22cc6c42682
nodonghui <163218508+nodonghui@users.noreply.github.com>
2026-04-14 10:17:53 +0900

    init commit

---
+++ react/docu_tag/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+ 
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>문서 저장 및 검색 시스템</h1>
+      </header>
+      <main>
+        <p>React 프로젝트가 시작되었습니다!</p>
+      </main>
+    </div>
+  );
+}
+ 
+export default App;
