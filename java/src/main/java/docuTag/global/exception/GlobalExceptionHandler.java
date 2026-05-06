72cc076bc843fc2ff33a5a53a861ff9c3ca915ad
nodonghui <163218508+nodonghui@users.noreply.github.com>
2026-05-06 17:41:06 +0900

    서버 내부 오류 디버깅 위해 임시 500 번 에러 반환 수정

--- java/src/main/java/docuTag/global/exception/GlobalExceptionHandler.java
+++ java/src/main/java/docuTag/global/exception/GlobalExceptionHandler.java
@@ -41,7 +41,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleException(Exception e) {
         return ResponseEntity
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .body(ErrorResponse.of(500, "서버 내부 오류가 발생했습니다"));
+                .body(ErrorResponse.of(500, e));
     }
 
     @ExceptionHandler(GeminiApiException.class)
